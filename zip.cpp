daa5c6a80e109eaebe847fe03822163f47c3deba
sheerluck <sheer.luck.andrew@gmail.com>
2013-08-16 10:09:38 +0400

    second step to http://ideone.com/3KtiAE

--- zip.cpp
+++ zip.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <regex>
-#include <algorithm>
+#include <deque>
 
 using namespace std;
 
-auto split = [](const string& a) -> vector<string> {
-    using size   = string::size_type;
-    auto found   = size{0};
-    auto current = size{0};
-    auto result  = vector<string>{};
+using container = deque<string>;
+auto split = [](string&& a) -> container {
+    auto result  = container{};
+    string::size_type found, current = 0;
     while ((found = a.find(",", current)) != string::npos) {
         result.emplace_back(a, current, found - current);
         current = found + 1;
@@ -19,40 +16,28 @@ auto split = [](const string& a) -> vector<string> {
     return result;
 };
 
+using ziptainer = deque<pair<string,string>>;
+ziptainer zip(container&& a, container&& b) {
+    if (a.empty() || b.empty()) return ziptainer{};
+    auto pop = [](container&& x) {
+        auto h = x.front();
+        x.pop_front();
+        return h;
+    };
+    auto ha = pop(forward<container>(a));
+    auto hb = pop(forward<container>(b));
+    auto result = zip(forward<container>(a), forward<container>(b));
+    result.push_front(make_pair(ha, hb));
+    return result;
+};
+
 int main()
 {
-    auto aa = string{"2,7,3,0,5,3,5"};
-    auto bb = string{"7,3,0,5,3,5,77777777777777"};
-    auto ta = split(aa);
-    auto tb = split(bb);
-
-    using big = unsigned long long;
-    auto add = [](const string a, const string b) {
-        auto from_string = [](const string x) {
-            stringstream ss(x);
-            auto value = big{0};
-            ss >> value;
-            return value;
-        };
-        auto x = from_string(a);
-        auto y = from_string(b);
-        return x + y;
-    };
-    cout << add("123","321") << endl;
-
-    auto zip = [&](const vector<string> a, const vector<string> b, decltype(add) func) {
-        return 100;
-    };
-
-    cout << zip(ta, tb, add) << endl;
-
-    /*
-    cout << aa << endl;
-    for( auto x: ta) cout << x << "a, ";
-    cout << endl << endl << bb << endl;
-    for( auto x: tb) cout << x << "b, ";
-    cout << endl;
-    */
+    cout << "[";
+    for(auto p: zip(split("2,7,3,0,5,3,5"), split("7,3,0,5,3,5,77777777777777"))) {
+        cout << stoull(p.first) + stoull(p.second) << ", ";
+    }
+    cout << "]\n";
 }
 
 
