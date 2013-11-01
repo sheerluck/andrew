1fb47ca2244f34a4a138dd42f2433c036f8459d0
sheerluck <sheer.luck.andrew@gmail.com>
2013-11-01 17:17:40 +0400

    Fix: puddle problem

--- puddles.py
+++ puddles.py
@@ -1,44 +1,32 @@
-def split_world(arr):
-    ma = max(arr)
-    indices = [i for i, x in enumerate(arr) if x == ma]
-    if 1 == len(indices):
-        if 0 == indices[0]: return split_world(arr[::-1])
-        return arr[: indices[0] + 1], arr[: indices[0] - 1: -1], []
-    chunks = []
-    for k in range(len(indices) - 1):
-        chunk = arr[indices[k]: indices[k + 1] + 1]
-        chunks.append(chunk)
-    return arr[: indices[0] + 1], arr[: indices[-1] - 1: -1], chunks
+def split_world(arr, max):
+    indices     = [i for i, x in enumerate(arr) if x == max]
+    if [0] == indices: return [], arr[: : -1], []  # leftmost
+    left, right = arr[: indices[0] + 1], arr[: indices[-1] - 1: -1]
+    chunks, ran = [], range(len(indices) - 1)
+    for k in ran: chunks.append(arr[indices[k]: indices[k + 1] + 1])
+    return left, right, chunks
 
 
 def puddles(arr):
-    if len(arr) < 3: return 0
-    ind = 0
-    while (arr[ind + 1] >= arr[ind]):
-        ind += 1
-        if len(arr) == ind + 1: return 0
-    left = ind
-    while (arr[ind + 1] < arr[left]): ind += 1
-    right = ind + 1
-    head, tail = arr[left:right + 1], arr[right:]
+    indx, length = 0, len(arr)
+    if length < 3: return 0
+    while (arr[indx + 1] >= arr[indx]):
+        indx += 1
+        if length == indx + 1: return 0
+    left = indx
+    while (arr[indx + 1] <  arr[left]): indx += 1
+    right = indx + 1
+    head, tail = arr[left: right + 1], arr[right:]
     head[-1] = head[0]
     return inner(head) + puddles(tail)
 
 
 def inner(arr):
-    if len(arr) < 3: return 0
-    ma = max(arr)
-    water = [ma - x for x in arr]
-    return sum(water)
+    return 0 if len(arr) < 3 else sum([arr[0] - x for x in arr])
 
 
-def main():
-    world = [2,5,1,3,1,2,1,7,6,3,1,3]
-    left, right, chunks = split_world(world)
-    inner_puddles = []
-    for chunk in chunks: inner_puddles.append(inner(chunk))
-    print world, "\nwater=", puddles(left) + puddles(right) + sum(inner_puddles)
-
-
-if __name__ == '__main__':
-    main()
+world = [2,5,1,3,1,2,1,7,6,3,1,3]
+left, right, chunks = split_world(world, max(world))
+inner_puddles = []
+for chunk in chunks: inner_puddles.append(inner(chunk))
+print world, "\nwater=", puddles(left) + puddles(right) + sum(inner_puddles)
