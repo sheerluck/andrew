7eb601ffe6c0b053bec6254c59b549d2f9be3529
sheerluck <sheer.luck.andrew@gmail.com>
2014-01-15 08:28:03 +0400

    Dive into C++11 (+5min)
    00:27:27

--- ball.cpp
+++ ball.cpp
@@ -15,9 +15,11 @@ void junk() {
 using namespace std;
 using namespace sf;
 
-constexpr int winW{800}, winH{600};
+constexpr int    winW{800},   winH{600};
 constexpr double ballR{10.0}, ballV{3.33};
 constexpr double padW{77.7},  padH{17.7},  padV{4.4};
+constexpr double brckW{55.5}, brckH{22.2};
+constexpr int    brckX{12},   brckY{6};
 
 template<typename Shape>
 struct Boo {
@@ -42,8 +44,9 @@ struct Boo {
 
 struct Pad : Boo<RectangleShape> {
     Pad(double mX, double mY)
-    : Boo{mX, mY, Color::Blue, {padW / 2,  padH / 2}, {0.0, 0.0} } { shape.setSize({padW,  padH}); } 
-    void update() {
+    : Boo{mX, mY, Color::Blue, {padW / 2, padH / 2}, {0.0, 0.0} }
+    { shape.setSize({padW, padH}); } 
+    virtual void update() {
         shape.move(velocity);
         velocity.x = 0; // stop
         auto LeftPressed = Keyboard::isKeyPressed(Keyboard::Key::Left);
@@ -55,15 +58,29 @@ struct Pad : Boo<RectangleShape> {
     virtual double offY() { return shape.getSize().y / 2; }
 }; // struct Pad
 
+struct Brck : Boo<RectangleShape> {
+    bool destroyed{false};
+    Brck(double mX, double mY)
+    : Boo{mX, mY, Color::Yellow, {brckW / 2, brckH / 2}, {0.0, 0.01} }
+    { shape.setSize({brckW, brckH}); } 
+    virtual void update() {
+        shape.move(velocity);
+    }
+    virtual double offX() { return shape.getSize().x / 2; }
+    virtual double offY() { return shape.getSize().y / 2; }
+}; // struct Brck
+
 struct Ball : Boo<CircleShape> {
+    bool nice{true};
     Ball(double mX, double mY)
-    : Boo{mX, mY, Color::Red, {ballR, ballR}, {-ballV, -ballV} }   { shape.setRadius(ballR);       }
-    void update() {
+    : Boo{mX, mY, Color::Red, {ballR, ballR}, {-ballV, -ballV} }
+    { shape.setRadius(ballR); }
+    virtual void update() {
         shape.move(velocity);
         if (left() <    0) velocity.x =  ballV;
         if (rite() > winW) velocity.x = -ballV;
         if (top()  <    0) velocity.y =  ballV;
-        if (bot()  > winH) velocity.y = -ballV;
+        if (bot()  > winH) nice = false;
     }
     virtual double offX() { return shape.getRadius(); }
     virtual double offY() { return shape.getRadius(); }
@@ -83,31 +100,45 @@ void test(A& p, B& b) {
         b.velocity.x = b.x() < p.x() ? -ballV : ballV;
         b.velocity.y = -ballV; // up
     }
-}
+} // void test(A& p, B& b)
+
+void fill_up_bricks_vector(vector<Brck>& br) {
+    // ah, use range(), Luke!
+    for(int X{0}; X < brckX; X++)
+        for(int Y{0}; Y < brckY; Y++) {
+            auto x = (X + 1) * (brckW + 3) + 22;
+            auto y = (Y + 1) * (brckH + 3);
+            br.emplace_back(x, y);
+        }
+} // void fill_up_bricks_vector(vector<Brck>& x)
 
 int main()
 {
     auto ball = Ball{winW / 2, winH / 2 };
     auto pad  = Pad {winW / 2, winH - 44};
+    auto brx  = vector<Brck>{};
+    fill_up_bricks_vector(brx);
 
     //auto win  = RenderWindow{ {winW, winH}, "Z" };  // error: call to implicitly-deleted copy constructor of 'sf::RenderWindow'
     RenderWindow win{ {winW, winH}, "Z" };
 
     win.setFramerateLimit(60);
 
-    while(true) {
+    while( ! Keyboard::isKeyPressed(Keyboard::Key::Escape)
+          && ball.nice ) {
+
         win.clear(Color::Black);
-        if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
+        win.draw(ball.shape);
+        win.draw(pad .shape);
+        for(auto& b : brx) win.draw(b.shape);
+        win.display();
 
         ball.update();
         pad .update();
+        for(auto& b : brx) b.update();
 
         test(pad, ball);
-
-        win.draw(ball.shape);
-        win.draw(pad .shape);
-        win.display();
     }
 }
 
-// 00:22:11
+// 00:27:27
