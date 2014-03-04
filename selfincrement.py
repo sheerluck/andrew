21b990df2ed0f3cee5a2bbc2910b46a9a94d8ad1
andrey_lukyanchuk <a.lukyanchuk@energostroytelecom.ru>
2014-03-04 11:53:40 +0400

    self modifying code

---
+++ selfincrement.py
@@ -0,0 +1,9 @@
+num = 1
+print num
+linebreak = '\r\n'
+with open(__file__, mode="rb") as f:
+    content    = f.read().split(linebreak)
+    def increment(boo): return str(1 + int(boo[1]))
+    content[0] = "num = " + increment(content[0].split(' = '))
+with open(__file__, mode="wb") as f:
+    f.write(linebreak.join(content))
