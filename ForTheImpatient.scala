c9ca70f3b839d8b25c7ab3f6ddcaccfb7d3c5a9d
sheerluck <sheer.luck.andrew@gmail.com>
2017-03-09 17:04:15 +0300

    OMG page 31 of 113

--- ForTheImpatient.scala
+++ ForTheImpatient.scala
@@ -27,5 +27,25 @@ r.toString(36)                                 // 2aon8rqll6x8ibdux7wz
 
 BigInt(2).pow(64)                              // 18446744073709551616
 
+for {
+  i <- 1 to 3
+  j <- 1 to 3
+  if i != j
+} print(s"${10 * i + j} ")                     // 12 13 21 23 31 32
+
+for (c <- "Hello"; i <- 0 to 1) yield (c + i).toChar  // HIeflmlmop
+for (i <- 0 to 1; c <- "Hello") yield (c + i).toChar  // Vector(H, e, l, l, o, I, f, m, m, p)
+
+def sum(args: Int*) = args.sum
+
+sum(1 to 100: _*)                              // 5050
+
+(10 to 0 by -1) foreach print                  // "downto": 109876543210 
+
+def countdown = (o:Int) => (o to 0 by -1) foreach println
+
+"Hello".map (_.toLong).product                 // Long = 9415087488 
+"Hello".map (_.toInt ).product                 // Int  =  825152896
+
 
 
