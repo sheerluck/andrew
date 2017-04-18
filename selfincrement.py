d1dd88319f37519f6e361a723836e073ebff3879
sheerluck <sheer.luck.andrew@gmail.com>
2017-04-18 11:05:51 +0300

    selfincrement @ linebreak

--- selfincrement.py
+++ selfincrement.py
@@ -1,6 +1,6 @@
 num = 1
 print num
-linebreak = '\r\n'
+linebreak = '\n'  #  '\r\n'
 with open(__file__, mode="rb") as f:
     content    = f.read().split(linebreak)
     def increment(boo): return str(1 + int(boo[1]))
