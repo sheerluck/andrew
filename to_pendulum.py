890d386193f1ac8c1db27ee81c5be7fcbd723831
sheerluck <sheer.luck.andrew@gmail.com>
2017-08-28 16:34:56 +0300

    line_profiler

--- to_pendulum.py
+++ to_pendulum.py
@@ -1,7 +1,14 @@
+from line_profiler import LineProfiler
 import pendulum
 from   inspect import getmembers
 
-dt    = pendulum.now()
-pairs = [(n, f) for (n, f) in getmembers(dt) if n.startswith("to_")]
-for name, method in pairs:
-    print(f"{name:>30}: '{method()}'")
+def main():
+    dt    = pendulum.now()
+    pairs = [(n, f) for (n, f) in getmembers(dt) if n.startswith("to_")]
+    for name, method in pairs:
+        print(f"{name:>30}: '{method()}'")
+
+lp = LineProfiler()
+lp_wrapper = lp(main)
+lp_wrapper()
+lp.print_stats()
