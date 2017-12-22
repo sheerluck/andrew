8d3bba2fadd95182dca2998c2ebca8ba1c33e817
sheerluck <sheer.luck.andrew@gmail.com>
2017-12-22 22:01:28 +0300

    space fix

--- valvar.cpp
+++ valvar.cpp
@@ -2,10 +2,7 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
-#include <numeric>
 #include <chrono>
-#include <list>
-#include <string>
 
 #define val const auto
 #define var auto
@@ -13,15 +10,15 @@
 int main() {
     using namespace std::string_literals;
 
-    val h = "Hello"s;
-    val w = "World"s;
-    val start = std::chrono::system_clock::now();
-    val vector = std::vector<int>{1, 2, 3, 4, 5};
-    var vect0r = std::vector<int>{10, 20, 30, 40, 50};
-    vect0r.insert(std::end(vect0r),
+    val hello  = "Hello"s;
+    val world  = "World"s;
+    val start  =  std::chrono::system_clock::now();
+    val vector =  std::vector<int>{10, 20, 30, 40, 50};
+    var vect0r =  std::vector<int>{ 1,  2,  3,  4,  5};
+    vect0r.insert(std::end(  vect0r),
                   std::begin(vector),
-                  std::end(vector));
-    val hw = h + w;
+                  std::end(  vector));
+    val hw = hello + world;
 
     for (val& t : boost::combine(hw, vect0r))
     {
@@ -30,8 +27,8 @@ int main() {
                   << "]\n"; 
     }
     
-    val end = std::chrono::system_clock::now();
-    val diff = std::chrono::duration<double>{end - start};
+    val end   =  std::chrono::system_clock::now();
+    val diff  =  std::chrono::duration<double>{end - start};
     std::cout << "Time: "
               << std::setprecision(25)
               << diff.count() << " s" << std::endl; 
