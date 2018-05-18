22d1c438754db3974f72697e66509ded9106e706
sheerluck <sheer.luck.andrew@gmail.com>
2018-05-18 16:03:41 +0300

    flake8 offline-twitter.py

--- offline-twitter.py
+++ offline-twitter.py
@@ -1,10 +1,8 @@
-#-*- coding: utf-8  -*-
-
-from PyQt5.QtCore    import Qt
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QLineEdit
-from os.path         import join as osjoin
-from datetime        import datetime
+from datetime import datetime
 import sys
+
 
 class XLineEdit(QLineEdit):
     def __init__(self, parent=None):
@@ -12,14 +10,13 @@ class XLineEdit(QLineEdit):
 
     def keyPressEvent(self, ev):
         def appendText(text):
-            root     = '/home/sheerluck/tmp/off'
-            today    = datetime.today()
+            today = datetime.today()
             day, hms = today.strftime("%Y-%m-%d"), today.strftime("%Y-%m-%d %H:%M:%S")
-            stamp    = ''    .join([ "[", hms, "]" ])
-            txt      = ' -> '.join([ stamp,   text ])
-            with open(osjoin(root, day + ".txt"), mode='a') as f:
-                f.write( txt+"\n" )
-        if ev.key() not in [Qt.Key_Return,  Qt.Key_Enter]:
+            stamp = ''.join(["[", hms, "]"])
+            txt = ' -> '.join([stamp, text])
+            with open(day + ".txt", mode='a') as f:
+                f.write(txt + "\n")
+        if ev.key() not in [Qt.Key_Return, Qt.Key_Enter]:
             QLineEdit.keyPressEvent(self, ev)
             return
         appendText(self.text())
