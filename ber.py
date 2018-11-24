1f4666a6ca2dc0c61c74db577138d93101471c31
sheerluck <sheer.luck.andrew@gmail.com>
2018-11-24 20:42:00 +0300

    ber

---
+++ ber.py
@@ -0,0 +1,30 @@
+from fractions import Fraction
+
+
+def take(n, it):
+    for a, b in zip(range(n), it):
+        yield b
+
+
+def bernoulli(n):
+    A = [Fraction(0)] * (n + 1)
+    for m in range(n + 1):
+        A[m] = Fraction(1, m + 1)
+        for j in range(m, 0, -1):
+            A[j - 1] = j * (A[j - 1] - A[j])
+    return A[0]
+
+
+def ber(n):
+    while True:
+        b = bernoulli(n)
+        if b:
+            nu, de = b.numerator, b.denominator
+            yield n, f"{nu}/{de}", nu/de
+        n += 1
+
+
+print(list(take(100, ber(0))))
+
+for a, b, c in ber(0):
+    print(f"B({a}) = {c} # {b}")
