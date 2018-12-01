961e9e36d6f76156a6cfad34d46290f05466ffda
sheerluck <sheer.luck.andrew@gmail.com>
2018-12-01 19:44:37 +0300

    value - prev

--- while-true-rdtsc.py
+++ while-true-rdtsc.py
@@ -3,7 +3,7 @@ from time import sleep
 from rdtsc import get_cycles
 while True:
     asap = [get_cycles() for _ in range(10)]
-    for value in asap:
-        print(value)
+    for value, prev in zip(asap, [asap[0]] + asap):
+        print(value, value - prev)
     print('-' * 15)
     sleep(0.001)  # to prevent 100% CPU
