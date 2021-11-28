e0855f92e6c7e2744d7636ec91708a50f719f935
sheerluck <sheer.luck.andrew@gmail.com>
2021-11-29 02:26:06 +0300

    it will eat all your memory so fast 😿

--- A033492.py
+++ A033492.py
@@ -1,3 +1,12 @@
+# https://oeis.org/A033492/list
+# 54      686
+# 55      689
+# 57      692
+# 58      705
+# 59      706
+# 60      745
+
+
 from colorama import Fore, init as colorama_init
 
 def forever(acc):
@@ -6,28 +15,47 @@ def forever(acc):
         acc += 1
 
 
-def hailstone(n):
-    seq = [n]
-    while n>1:
-        n = 3*n + 1 if n & 1 else n//2
-        seq.append(n)
-    return seq
+def flatten(a, b): 
+    (L, tail), mem = b
+    i = 1
+    flat = [0] * L
+    flat[0] = a
+    while i < L:
+        L1, t1 = mem[tail]
+        flat[i] = tail
+        tail = t1
+        i += 1
+    return flat
+
+def hailstone(n, m):
+    nn = 3*n + 1 if n & 1 else n//2
+    if nn in m:
+        L, tail = m[nn]
+    else:
+        L, tail = hailstone(nn, m)
+    m[n] = (L+1, nn)
+    return m[n]
 
 
 class seq:
     def __init__(self, a):
         self.a = a
+        self.mem = {1: (1, None), 2: (2, 1)}
 
     def __iter__(self):
         while True:
-            yield hailstone(self.a)
+            if self.a in self.mem:
+                h = self.mem[self.a]
+            else:
+                h = hailstone(self.a, self.mem)
+            yield h, self.mem
             self.a += 1
 
 colorama_init(autoreset=True)
 prev = 0
 for a, b in zip(forever(1), seq(1)):
-    c = len(b)
+    (c, tail), mem = b
     if c > prev:
         print(Fore.CYAN + f"[{c:>10}]", end=" ")
-        print(b, end="\n\n")
+        print(flatten(a, b), end="\n\n")
         prev = c
