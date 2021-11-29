7a831630b63733746e9752cd99843bfa5d8115d0
sheerluck <sheer.luck.andrew@gmail.com>
2021-11-29 14:09:03 +0300

    slow, but not wasting memory to store length

--- A033492.py
+++ A033492.py
@@ -5,9 +5,11 @@
 # 58      705
 # 59      706
 # 60      745
+# 61      950
 
 
 from colorama import Fore, init as colorama_init
+
 
 def forever(acc):
     while True:
@@ -15,32 +17,28 @@ def forever(acc):
         acc += 1
 
 
-def flatten(a, b): 
-    (L, tail), mem = b
-    i = 1
-    flat = [0] * L
-    flat[0] = a
-    while i < L:
-        L1, t1 = mem[tail]
-        flat[i] = tail
+def flatten(a, b):
+    tail, mem = b
+    flat = [a]
+    while tail:
+        t1 = mem[tail]
+        flat.append(tail)
         tail = t1
-        i += 1
     return flat
+
 
 def hailstone(n, m):
     nn = 3*n + 1 if n & 1 else n//2
-    if nn in m:
-        L, tail = m[nn]
-    else:
-        L, tail = hailstone(nn, m)
-    m[n] = (L+1, nn)
+    if nn not in m:
+        hailstone(nn, m)
+    m[n] = nn
     return m[n]
 
 
 class seq:
     def __init__(self, a):
         self.a = a
-        self.mem = {1: (1, None), 2: (2, 1)}
+        self.mem = {1: None, 2: 1}
 
     def __iter__(self):
         while True:
@@ -51,11 +49,12 @@ class seq:
             yield h, self.mem
             self.a += 1
 
+
 colorama_init(autoreset=True)
 prev = 0
 for a, b in zip(forever(1), seq(1)):
-    (c, tail), mem = b
-    if c > prev:
+    flat = flatten(a, b)
+    if (c := len(flat)) > prev:
         print(Fore.CYAN + f"[{c:>10}]", end=" ")
-        print(flatten(a, b), end="\n\n")
+        print(flat, end="\n\n")
         prev = c
