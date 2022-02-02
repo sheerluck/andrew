0df556585c40d41efac024b77b6dafe8b647b731
sheerluck <sheer.luck.andrew@gmail.com>
2022-02-02 13:41:30 +0300

    iso-2022-jp-2004

--- names.py
+++ names.py
@@ -1,10 +1,18 @@
 # $ python names.py | less
 from unicodedata import name
-for num in range(131_000):
+for num in range(200_000):
     try:
         c = chr(num)
         n = name(c)
+        jp = ""
+        try:
+            jp = c.encode('iso-2022-jp-2004')
+        except:
+            pass
     except:
         pass
     else:
-        print(f"{num:>8} {c}: {n}")
+        if len(jp) in [8,9]:
+            print(f"{num:>8} {c}: {n:<50} || {jp}")
+        else:
+            print(f"{num:>8} {c}: {n}")
